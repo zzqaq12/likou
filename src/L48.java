2ebceaa8fdd73705a6500ec940e14035df906ca7
zhouwang <zhouwang@wisecotech.com>
2024-07-02 11:33:42 +0800

    48 旋转图像

---
+++ src/L48.java
@@ -0,0 +1,24 @@
+public class L48 {
+    /**
+     * 给定一个 n × n 的二维矩阵 matrix 表示一个图像。请你将图像顺时针旋转 90 度。
+     *
+     * 你必须在 原地 旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要 使用另一个矩阵来旋转图像。
+     */
+    public static void main(String[] args) {
+
+    }
+    public static void rotate(int[][] matrix) {
+        //旋转图像
+        int n = matrix.length;
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = 0; j < (n + 1) / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - 1 - j][i];
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
+                matrix[n - 1 - i][n - 1 -j] = matrix[j][n - 1 - i];
+                matrix[j][n - 1 - i] = temp;
+            }
+        }
+    }
+
+}
