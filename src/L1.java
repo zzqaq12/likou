59d5bcced74154ab960bae006e97f8ca416b48c4
zhouwang <zhouwang@wisecotech.com>
2024-07-08 11:37:52 +0800

    1 两数之和

---
+++ src/L1.java
@@ -0,0 +1,26 @@
+public class L1 {
+    /**
+     * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+     *
+     * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+     *
+     * 你可以按任意顺序返回答案。
+     */
+    public static void main(String[] args) {
+
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        System.out.println(twoSum(nums,target));
+    }
+    public static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return null;
+    }
+
+}
