e9bf39ba7656b7a7dfe189fdd2b36726f5fc3cdd
zhouwang <zhouwang@wisecotech.com>
2024-07-08 11:24:22 +0800

    242 有效的字母异位词

---
+++ src/L242.java
@@ -0,0 +1,29 @@
+public class L242 {
+    /**
+     * 给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+     *
+     * 注意：若 s 和 t 中每个字符出现的次数都相同，则称 s 和 t 互为字母异位词。
+     */
+    public static void main(String[] args) {
+
+        System.out.println(isAnagram("anagram", "nagaram"));
+    }
+
+    public static boolean isAnagram(String s, String t) {
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] arr = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            arr[s.charAt(i) - 'a']++;
+            arr[t.charAt(i) - 'a']--;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
