959ba2ea0a478a464b75720f64c05325bad3f45b
zhouwang <zhouwang@wisecotech.com>
2024-07-25 10:14:36 +0800

    135 搜索插入位置

---
+++ src/L35.java
@@ -0,0 +1,33 @@
+public class L35 {
+    /**
+     * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+     *
+     * 请必须使用时间复杂度为 O(log n) 的算法。
+     */
+    public static void main(String[] args) {
+
+        int[] nums = {1,3,5,6};
+        System.out.println(searchInsert(nums, 7));
+    }
+    public static int searchInsert(int[] nums, int target) {
+
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target)
+            {
+                return mid;
+            }
+            else if (nums[mid] > target)
+            {
+                right = mid - 1;
+            }
+            else
+            {
+               left = mid + 1;
+           }
+       }
+        return left;
+   }
+}
