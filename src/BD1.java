14eaf306309f9f855e7370aece8fcf59dd3c508c
zzqaq12 <974762470@qq.com>
2024-08-06 22:12:52 +0800

    BD1

--- src/BD1.java
+++ src/BD1.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class BD1 {
     /**
      * 找到字符串中，重复字符不超过K个的最长子串
@@ -10,47 +13,37 @@ public class BD1 {
         System.out.println(longestSubstring(s, 2));
     }
 
-    public static String longestSubstring(String s, int k) {
+    public static String longestSubstring(String str, int k) {
 
-        if (s == null || s.length() == 0 || k <= 0) {
+        if (str == null || str.length() == 0 || k <= 0) {
             return "";
         }
-
         // 统计每个字符出现的次数
-        int[] count = new int[26];
-        for (int i = 0; i < s.length(); i++) {
-            count[s.charAt(i) - 'a']++;
-        }
+        Map<Character, Integer> map = new HashMap<>();
+        int left = 0; // 滑动窗口的左边界
+        int maxLength = 0; // 最长子串的长度
+        String longestSubstring = ""; // 最长子串
+        for (int right = 0; right < str.length(); right++) {
+            map.put(str.charAt(right), map.getOrDefault(str.charAt(right), 0) + 1);
+            // 如果窗口中字符的个数大于k，移动左边界
+            while (map.size() > k) {
+                map.put(str.charAt(left), map.get(str.charAt(left)) - 1);
+                // 如果移除后，窗口中字符的个数小于k，则窗口左边界向右移动
+                if (map.get(str.charAt(left)) == 0) {
+                    map.remove(str.charAt(left));
 
-        char[] chars = s.toCharArray();
-        int left = 0;
-        int right = 0;
-        int maxLen = 0;
-        int maxLeft = 0;
-        int maxRight = 0;
-        while (right < chars.length) {
-            // 如果当前字符的个数大于等于k，则右指针右移
-            if (count[chars[right] - 'a'] >= k) {
-                right++;
-            } else {
-                if (right - left > maxLen) {
-                    maxLen = right - left;
-                    maxLeft = left;
-                    maxRight = right;
                 }
-                // 当前字符的个数小于k，则左指针右移
-                left = right + 1;
-                right++;
+                // 更新最长子串的长度和子串
+                if (right - left + 1 > maxLength) {
+                    maxLength = right - left + 1;
+                    longestSubstring = str.substring(left, right);
+                }
+                left++;
+
+
             }
         }
-
-        if (right - left > maxLen) {
-            maxLen = right - left;
-            maxLeft = left;
-            maxRight = right;
-        }
-
-        return s.substring(maxLeft, maxRight);
-
+        return longestSubstring;
     }
+
 }
