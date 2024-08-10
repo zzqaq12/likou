34742282a799d953c82c5f31f2f0b04d7a123529
zzqaq12 <974762470@qq.com>
2024-08-10 22:09:48 +0800

    104 二叉树最大深度

---
+++ src/L104.java
@@ -0,0 +1,38 @@
+public class L104 {
+
+    /**
+     * 给定一个二叉树 root ，返回其最大深度。
+     *
+     * 二叉树的 最大深度 是指从根节点到最远叶子节点的最长路径上的节点数。
+     */
+
+    public static void main(String[] args) {
+
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+        System.out.println(maxDepth(root));
+    }
+
+    public static int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+     public static class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+      }
+
+}
