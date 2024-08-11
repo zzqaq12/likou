19b52ec014f38331edf9a2fc812af4b67a4a660b
zzqaq12 <974762470@qq.com>
2024-08-11 15:57:51 +0800

    114 二叉树展开为链表

---
+++ src/L114.java
@@ -0,0 +1,44 @@
+public class L114 {
+
+    /**
+     * 给你二叉树的根结点 root ，请你将它展开为一个单链表：
+     *
+     * 展开后的单链表应该同样使用 TreeNode ，其中 right 子指针指向链表中下一个结点，而左子指针始终为 null 。
+     * 展开后的单链表应该与二叉树 先序遍历 顺序相同。
+     */
+    public static void main(String[] args) {
+    }
+
+    public static void flatten(TreeNode root) {
+        if (root == null) return;
+        TreeNode cur = root;
+        while (cur != null) {
+            if (cur.left != null) {
+                TreeNode pre = cur.left;
+                while (pre.right != null) {
+                    pre = pre.right;
+                }
+                pre.right = cur.right;
+                cur.right = cur.left;
+                cur.left = null;
+                cur = cur.right;
+            } else {
+                cur = cur.right;
+            }
+
+        }
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
