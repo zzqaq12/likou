693920799c08cb429728188f0e8712123b8b6724
zzqaq12 <974762470@qq.com>
2024-08-11 16:53:50 +0800

    119 求根节点到叶节点数字之和

---
+++ src/L129.java
@@ -0,0 +1,41 @@
+public class L129 {
+    /**
+     * 给你一个二叉树的根节点 root ，树中每个节点都存放有一个 0 到 9 之间的数字。
+     * 每条从根节点到叶节点的路径都代表一个数字：
+     *
+     * 例如，从根节点到叶节点的路径 1 -> 2 -> 3 表示数字 123 。
+     * 计算从根节点到叶节点生成的 所有数字之和 。
+     *
+     * 叶节点 是指没有子节点的节点。
+     */
+    public static void main(String[] args) {
+
+    }
+
+    public static int sumNumbers(TreeNode root) {
+        return dfs(root, 0);
+    }
+
+    public static int dfs(TreeNode root, int sum) {
+        if (root == null) {
+            return 0;
+        }
+        sum = sum * 10 + root.val;
+        if (root.left == null && root.right == null) {
+            return sum;
+        }
+        return dfs(root.left, sum) + dfs(root.right, sum);
+    }
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
