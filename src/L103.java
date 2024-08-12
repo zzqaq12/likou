87f7a2e139ad076270b5fde4b21442b539460126
zzqaq12 <974762470@qq.com>
2024-08-12 22:14:32 +0800

    103 二叉树的锯齿形层序遍历

---
+++ src/L103.java
@@ -0,0 +1,70 @@
+import java.util.*;
+
+public class L103 {
+    /**
+     * 给你二叉树的根节点 root ，返回其节点值的 锯齿形层序遍历 。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
+     */
+
+    public static void main(String[] args) {
+
+    }
+
+    public static List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        Stack stack = new Stack<Integer>();
+        boolean flag = true;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            if (flag == true) {
+                for (int i = 0; i < size; i++) {
+                    TreeNode node = queue.poll();
+                    list.add(node.val);
+                    if (node.left != null) {
+                        queue.offer(node.left);
+                    }
+                    if (node.right != null) {
+                        queue.offer(node.right);
+                    }
+                }
+            }
+            else {
+                for (int i = 0; i < size; i++) {
+                    TreeNode node = queue.poll();
+                    stack.push(node.val);
+                    if (node.left != null) {
+                        queue.offer(node.left);
+                    }
+                    if (node.right != null) {
+                        queue.offer(node.right);
+                    }
+                }
+                while (!stack.isEmpty()) {
+                    list.add((int) stack.pop());
+               }
+            }
+            res.add(list);
+            flag = !flag;
+
+        }
+        return res;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
